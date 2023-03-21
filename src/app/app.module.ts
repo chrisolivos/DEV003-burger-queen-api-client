c19a4a2ba7fba1e99595ff69311f71e5abd1101f
Chris <chris.olivosf@gmail.com>
2023-03-20 21:19:43 -0300

    estructurando modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,27 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BurgerQueenModule } from './burger-queen/burger-queen.module';
+import { LoginBqModule } from './login-bq/login-bq.module';
+import { HomeBqModule } from './home-bq/home-bq.module';
+import { RegisterBqModule } from './register-bq/register-bq.module';
+import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BurgerQueenModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LoginBqModule,
+    HomeBqModule,
+    RegisterBqModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-bq/home-bq.module.ts
+++ src/app/home-bq/home-bq.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[
-    LoginComponent
+  exports: [
+    HomeComponent
   ]
 })
-export class BurgerQueenModule { }
+export class HomeBqModule { }
