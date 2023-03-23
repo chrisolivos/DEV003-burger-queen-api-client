b1778e648f27e1400b3423fa2cdc0f5f3759fd13
Chris <chris.olivosf@gmail.com>
2023-03-23 17:04:28 -0300

    delete signin

--- src/app/auth-bq/auth-bq.module.ts
+++ src/app/auth-bq/auth-bq.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { AuthBqRoutingModule } from './auth-bq-routing.module';
 import { LoginComponent } from './login/login.component';
-import { SiginComponent } from './sigin/sigin.component';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 
@@ -11,8 +10,6 @@ import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     LoginComponent,
-    SiginComponent,
-
   ],
   imports: [
     CommonModule,
@@ -21,8 +18,7 @@ import { ReactiveFormsModule,FormsModule } from '@angular/forms';
     FormsModule
   ],
   exports:[
-    LoginComponent,
-    SiginComponent
+    LoginComponent
   ]
 })
 export class AuthBqModule { }
