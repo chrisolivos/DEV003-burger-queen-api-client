c98631620da5ff54b807531896965ce6a56b4052
YsisC <ysisclongart@gmail.com>
2023-03-24 13:33:10 -0300

    css home

--- src/app/home-bq/home-bq.module.ts
+++ src/app/home-bq/home-bq.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from '../component/header/header.component';
 import { HomeBqRoutingModule } from './home-bq-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ComponentModule } from '../component/component.module';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+   
   ],
   imports: [
     CommonModule,
-    HomeBqRoutingModule
+    HomeBqRoutingModule,
+    ComponentModule
   ]
 })
 export class HomeBqModule { }
