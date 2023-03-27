1026416b63302406641d87c047a985921d90f0b8
YsisC <ysisclongart@gmail.com>
2023-03-27 14:53:39 -0300

    instalando browser animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AuthBqModule } from './auth-bq/auth-bq.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -19,7 +18,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AuthBqModule,
+
   
   ],
   providers: [],
