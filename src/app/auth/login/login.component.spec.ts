145e4b6005273d86210d6dfc9f03cc5082ed7d1d
YsisC <ysisclongart@gmail.com>
2023-03-28 11:58:39 -0300

    integrar v2

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -7,6 +12,14 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        ReactiveFormsModule,
+        FormsModule,
+        ToastrModule.forRoot(),
+        HttpClientTestingModule,
+        BrowserModule,
+        ToastrModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
