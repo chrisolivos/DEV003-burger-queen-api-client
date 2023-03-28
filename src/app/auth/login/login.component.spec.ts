5f3f4273ae7205b853cd92780b9b610b7d1bcaa2
Chris <chris.olivosf@gmail.com>
2023-03-28 09:57:13 -0300

    pruebas de test

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+//import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import { HttpClientModule } from '@angular/common/http';
+//import { Router } from '@angular/router';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +12,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      // imports:[
+      //   FormsModule, ReactiveFormsModule,
+      //   HttpClientModule
+      // ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -20,4 +28,29 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+it('Debe retornar formulario invalido',()=>{
+  const fixture = TestBed.createComponent(LoginComponent);
+  const app = fixture.componentInstance;
+  fixture.detectChanges();
+
+  const form = app.login;
+  const email = app.email;
+  email.setValue('admin@gmail.com');
+  expect(form.invalid).toBeTrue();
+})
+
+// it('Debe retornar formulario valido',()=>{
+//   const fixture = TestBed.createComponent(LoginComponent);
+//   const app = fixture.componentInstance;
+//   fixture.detectChanges();
+
+//   const form = app.login;
+//   const email = app.email;
+//  const password =app.password;
+//   email.setValue('amin@gmail.com');
+//  password.setValue('123456');
+//   expect(form.invalid).toBeFalse();
+// })
+
 });
