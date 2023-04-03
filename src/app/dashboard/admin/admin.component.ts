38f8c508119dd75b2a972afc2b8c69f3c72dc91b
YsisC <ysisclongart@gmail.com>
2023-04-03 15:20:10 -0300

    Crear boton de editar

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -14,10 +14,15 @@ import { ApiService } from '../../shared/api.service';
 
 
 export class AdminComponent {
-  usuarios = '';
-
+  //Declaracion de Variables
+  usuarios:string = '';
+  employeeModelObj: EmployedModel = new EmployedModel();
+  employeeData !:any;
+  userList: any;
+  dataSource:any
 
   url = 'http://localhost:5000/users';
+
   constructor(private route: Router,private http: HttpClient, private api: ApiService) {
 
 
@@ -25,6 +30,8 @@ export class AdminComponent {
   ngOnInit(): void {
    this.getAllEmpoyee();
   }
+
+  //Formulario de resistro
    signup = new FormGroup({
     'email': new FormControl('', [Validators.required, Validators.email]),
     'password': new FormControl('',  [Validators.required, Validators.minLength(6), 
@@ -34,10 +41,8 @@ export class AdminComponent {
     
   });
 
-  userList: any;
-  dataSource:any
-  EmployedModelObj: EmployedModel = new EmployedModel();
-  employeeData !:any;
+
+
   LoadUser(){
     this.http.get(this.url).subscribe(res => {
     this.userList = res;
@@ -64,10 +69,52 @@ export class AdminComponent {
       ;
 
   }
-  getAllEmpoyee(){
+  
+getAllEmpoyee(){
     this.api.getEmploye()
     .subscribe(res=>{
     this.employeeData=res;
     })
-  }
 }
+
+deleteEmployee(row: any){
+  this.api.deleteEmploye(row.id)
+  .subscribe(res=>{
+    alert( 'Empleado borrado')
+    this.getAllEmpoyee()
+  })
+}
+
+onEdit(row: any){
+  this.signup.controls['email'].setValue(row.email);
+  this.signup.controls['password'].setValue(row.password);
+  this.signup.controls['rol'].setValue(row.rol)
+  this.signup.controls['adminaccess'].setValue(row.adminacces)
+}
+
+updateEmployeeDetails(){
+  if(this.signup.value.email!==undefined||this.signup.value.email !==null){
+    this.employeeModelObj.email = this.signup.value.email!;
+  }
+  if(this.signup.value!==undefined||this.signup.value !==null){
+ 
+    this.employeeModelObj.password = this.signup.value.password!;
+    this.employeeModelObj.rol = this.signup.value.rol;
+    this.employeeModelObj.adminaccess = this.signup.value.adminaccess!;
+    this.api.updateEmployee(this.employeeModelObj, this.employeeModelObj.id)
+    .subscribe(res=>{
+      let ref = document.getElementById('cancel');
+      ref?.click()
+      this.signup.reset()
+      this.getAllEmpoyee()
+    })
+  }
+ 
+
+ 
+
+
+}
+ 
+}
+
